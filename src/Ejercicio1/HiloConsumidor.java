edd9f9027cc5149efdb8650f654d73a6b4c87d47
Pablo <Pablo@DESKTOP-U7VPSPJ>
2024-05-01 18:02:51 +0200

    Vamos compi

---
+++ src/Ejercicio1/HiloConsumidor.java
@@ -0,0 +1,11 @@
+package Ejercicio1;
+
+public class HiloConsumidor extends Thread {
+	private int id;
+	private int op;
+	
+	@Override
+	public void run() {
+		
+	}
+}
---
+++ src/Ejercicio1/VariablesCompartidas.java
@@ -0,0 +1,11 @@
+package Ejercicio1;
+
+public class VariablesCompartidas {
+	private int[] numOp;
+	private int[] results;
+	
+	public VariablesCompartidas() {
+		numOp = new int[110];
+		results = new int[10];
+	}
+}
